ccf7c5045a1cb398ead174abc308587fe399a1cd
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2025-01-10 19:58:38 +0200

    Configure autoload for appointment management module

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,10 +3,11 @@
 use Modules\DoctorAvailability\Providers\DoctorAvailabilityServiceProvider;
 use Modules\AppointmentBooking\Providers\AppointmentBookingServiceProvider;
 use Modules\AppointmentConfirmation\Providers\AppointmentConfirmationServiceProvider;
-
+use Modules\AppointmentManagement\Providers\AppointmentManagementServiceProvider;
 return [
     App\Providers\AppServiceProvider::class,
     DoctorAvailabilityServiceProvider::class,
     AppointmentBookingServiceProvider::class,
     AppointmentConfirmationServiceProvider::class,
+    AppointmentManagementServiceProvider::class,
 ];
---
+++ modules/AppointmentManagement/Routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/appointment-management', function () {
+    return 'Appointment Management';
+});
