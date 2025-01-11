f4f770b887031058ed9437c16289081954d11cdd
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2025-01-11 17:25:30 +0200

    Update service provider and route provider with migration and route file paths

--- modules/AppointmentBooking/Providers/AppointmentBookingServiceProvider.php
+++ modules/AppointmentBooking/Providers/AppointmentBookingServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Modules\AppointmentBooking\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Modules\AppointmentBooking\Domain\Repositories\AppointmentRepositoryInterface;
+use Modules\AppointmentBooking\Infrastructure\Repositories\AppointmentRepository;
 
 class AppointmentBookingServiceProvider extends ServiceProvider
 {
@@ -11,11 +13,11 @@ class AppointmentBookingServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../Infrastructure/Database/Migrations');
         $this->mergeConfigFrom(__DIR__.'/../Config/config.php', 'appointment-booking');
         $this->app->register(RouteServiceProvider::class);
 
         // bind to container
-        // $this->app->bind(ExampleRepositoryInterface::class, ExampleRepository::class);
+        $this->app->bind(AppointmentRepositoryInterface::class, AppointmentRepository::class);
     }
 }
