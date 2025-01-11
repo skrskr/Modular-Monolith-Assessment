2c14735926b4ddbd9256a5ab937c2a30645e6c6f
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2025-01-11 23:14:23 +0200

    Doctor Availability module - fix typos

--- modules/DoctorAvailability/Http/Controllers/SlotController.php
+++ modules/DoctorAvailability/Http/Controllers/SlotController.php
@@ -3,32 +3,31 @@
 namespace Modules\DoctorAvailability\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Modules\DoctorAvailability\Services\SlotService;
 use Modules\DoctorAvailability\Http\Requests\CreateSlotRequest;
+use Modules\DoctorAvailability\Services\SlotService;
 
 class SlotController extends Controller
 {
-    protected $slotService;
 
-    public function __construct(SlotService $slotService)
-    {
-        $this->slotService = $slotService;
-    }
+    public function __construct(protected SlotService $slotService)
+    {}
 
     /**
      * Get a list of available slots for a doctor.
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function listAvialableSlots()
+    public function listAvailableSlots()
     {
-        $slots = $this->slotService->listAvialableSlots();
+        $slots = $this->slotService->listAvailableSlots();
+
         return response()->json($slots);
     }
 
     public function store(CreateSlotRequest $request)
     {
         $slot = $this->slotService->createSlot($request->validated());
+
         return response()->json($slot, 201);
     }
 }
--- modules/DoctorAvailability/Routes/api.php
+++ modules/DoctorAvailability/Routes/api.php
@@ -6,6 +6,6 @@ use Modules\DoctorAvailability\Http\Controllers\SlotController;
 Route::prefix('/doctor-availability', function () {
     Route::prefix('slots')->group(function () {
         Route::post('/', [SlotController::class, 'create'])->name('doctor-availability-create-slot');
-        Route::get('/available', [SlotController::class, 'listAvialableSlots'])->name('doctor-availability-list-available-slots');
+        Route::get('/available', [SlotController::class, 'listAvailableSlots'])->name('doctor-availability-list-available-slots');
     });
 });
